6258fac7c20511ad22f5724bc1621302f6e728e1
Alfie <53308938+philldesign@users.noreply.github.com>
2019-09-25 15:16:18 -0400

    Input Values are rendered on ttrain table after submit

--- javascript/main.js
+++ javascript/main.js
@@ -19,6 +19,7 @@
 //   firebase.initializeApp(firebaseConfig);
 // </script>
 
+
 var firebaseConfig = {
     apiKey: "AIzaSyB7mq0_BFZreqng_FrdKml5MyMcE6kFLRA",
     authDomain: "train-schedule-55681.firebaseapp.com",
@@ -30,45 +31,51 @@ var firebaseConfig = {
 };
 
 // firebase.initializeApp(config);
+    
+
+// $("#traininput").on("submit", function (event) {
+//     event.preventDefault();
+
+//     database.ref().push({
+//         name: name,
+//         destination: destination,
+//         first: first,
+//         frequency: frequency
+//     });
 
 
-$("#traininput").on("submit", function (event) {
-    event.preventDefault();
+//     $("#train-name").val("");
+//     $("#train-destination").val("");
+//     $("#first-time").val("");
+//     $("#frequency").val("");
 
-    var name = $("#train-name").val().trim();
-    var destination = $("#train-destination").val().trim();
-    var first = $("#first-time").val().trim();
-    var frequency = $("#frequency").val().trim();
-
-    database.ref().push({
-        name: name,
-        destination: destination,
-        first: first,
-        frequency: frequency
-    });
-
-
-    $("#train-name").val("");
-    $("#train-destination").val("");
-    $("#first-time").val("");
-    $("#frequency").val("");
+//     return false;
 
 
 
-});
+// });
 
 // Function To add Row To Train Table When Submit Button is Clicked // *
 
 $("#submit").on("click", function (event) {
     event.preventDefault();
 
+// Why if I put the variables below outside this function the values are not placed in the columns when 
+// submit button is pressed? 
+
+    var name = $("#train-name").val().trim();
+    var destination = $("#train-destination").val().trim();
+    var first = $("#first-time").val().trim();
+    var frequency = $("#frequency").val().trim();
+    
+
     var addRow = $("<tr>");
     $("#last").append(addRow);
 
-    var col1 = $("<td>").text(1);
-    var col2 = $("<td>").text(2);
-    var col3 = $("<td>").text(3);
-    var col4 = $("<td>").text(4);
+    var col1 = $("<td>").text(name);
+    var col2 = $("<td>").text(destination);
+    var col3 = $("<td>").text(first);
+    var col4 = $("<td>").text(frequency);
     var col5 = $("<td>").text(5);
    
 
@@ -77,14 +84,12 @@ $("#submit").on("click", function (event) {
     .append(col2)
     .append(col3)
     .append(col4)
-    .append(col5)
+    .append(col5);
 
 });
-
-
-
 
 var now = moment();
 console.log(now);
 $("#traininput").append($("<div id='clock'>"));
 $("#clock").text(now);
+
