4a7c22f7ee07276fb9ece37aa6978494ff5add3f
Alfie <53308938+philldesign@users.noreply.github.com>
2019-09-25 14:45:17 -0400

    Submit btn & row additions

--- javascript/main.js
+++ javascript/main.js
@@ -29,12 +29,62 @@ var firebaseConfig = {
     appId: "1:669334403620:web:02515655ef4fe8601db6b1"
 };
 
-firebase.initializeApp(config);
+// firebase.initializeApp(config);
 
 
-$("#formID").on("submit", function (event) {
+$("#traininput").on("submit", function (event) {
     event.preventDefault();
 
+    var name = $("#train-name").val().trim();
+    var destination = $("#train-destination").val().trim();
+    var first = $("#first-time").val().trim();
+    var frequency = $("#frequency").val().trim();
 
-})
+    database.ref().push({
+        name: name,
+        destination: destination,
+        first: first,
+        frequency: frequency
+    });
 
+
+    $("#train-name").val("");
+    $("#train-destination").val("");
+    $("#first-time").val("");
+    $("#frequency").val("");
+
+
+
+});
+
+// Function To add Row To Train Table When Submit Button is Clicked // *
+
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+
+    var addRow = $("<tr>");
+    $("#last").append(addRow);
+
+    var col1 = $("<td>").text(1);
+    var col2 = $("<td>").text(2);
+    var col3 = $("<td>").text(3);
+    var col4 = $("<td>").text(4);
+    var col5 = $("<td>").text(5);
+   
+
+    addRow
+    .append(col1)
+    .append(col2)
+    .append(col3)
+    .append(col4)
+    .append(col5)
+
+});
+
+
+
+
+var now = moment();
+console.log(now);
+$("#traininput").append($("<div id='clock'>"));
+$("#clock").text(now);
