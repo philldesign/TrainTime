70fa5665b5a501cca7785b56188d21855ff24b6a
Alfie <53308938+philldesign@users.noreply.github.com>
2019-09-24 23:27:04 -0400

    Table and Firebase  setup done

---
+++ javascript/main.js
@@ -0,0 +1,34 @@
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/6.6.2/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+
+// <script>
+//   // Your web app's Firebase configuration
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyB7mq0_BFZreqng_FrdKml5MyMcE6kFLRA",
+//     authDomain: "train-schedule-55681.firebaseapp.com",
+//     databaseURL: "https://train-schedule-55681.firebaseio.com",
+//     projectId: "train-schedule-55681",
+//     storageBucket: "train-schedule-55681.appspot.com",
+//     messagingSenderId: "669334403620",
+//     appId: "1:669334403620:web:02515655ef4fe8601db6b1"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+// </script>
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB7mq0_BFZreqng_FrdKml5MyMcE6kFLRA",
+    authDomain: "train-schedule-55681.firebaseapp.com",
+    databaseURL: "https://train-schedule-55681.firebaseio.com",
+    projectId: "train-schedule-55681",
+    storageBucket: "train-schedule-55681.appspot.com",
+    messagingSenderId: "669334403620",
+    appId: "1:669334403620:web:02515655ef4fe8601db6b1"
+  };
+
+
+
+  
